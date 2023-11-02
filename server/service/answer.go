b8e71f03cbf5eef0970fb7a7701b125eee345af5
sxz799 <sxz799@126.com>
2023-11-02 20:17:25 +0800

    完善后端添加答案逻辑 完善前端答题页面显示效果

--- server/service/answer.go
+++ server/service/answer.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/sxz799/surveyX/model/entity"
 	"github.com/sxz799/surveyX/utils"
-	"log"
 )
 
 type AnswerService struct {
@@ -13,30 +12,28 @@ type AnswerService struct {
 func (ts *AnswerService) Add(as []entity.Answer) (err error) {
 	var survey entity.Survey
 	var surveyId int
+	var contact, finger string
 	if len(as) > 0 {
 		surveyId = as[0].SurveyId
 		err = utils.DB.Where("id = ?", surveyId).First(&survey).Error
 		if err != nil {
 			return
 		}
+		contact = as[0].Contact
+		finger = as[0].Finger
 	}
 	repeat := survey.Repeat
 	check := survey.RepeatCheck
 	switch repeat {
-
 	case "no":
 		switch check {
 		case "contact":
-			contact := as[0].Contact
 			var num int64
 			utils.DB.Debug().Model(&entity.Answer{}).Where("contact = ? and survey_id=?", contact, surveyId).Count(&num)
-			log.Println(num)
 			if num > 0 {
-
 				return errors.New("该问卷不允许重复提交")
 			}
 		case "finger":
-			finger := as[0].Finger
 			var num int64
 			utils.DB.Model(&entity.Answer{}).Where("finger = ? and survey_id=?", finger, surveyId).Count(&num)
 			if num > 0 {
@@ -46,10 +43,8 @@ func (ts *AnswerService) Add(as []entity.Answer) (err error) {
 	case "yes_but_update":
 		switch check {
 		case "contact":
-			contact := as[0].Contact
 			utils.DB.Model(&entity.Answer{}).Delete(&entity.Answer{}, "contact = ? and survey_id=?", contact, surveyId)
 		case "finger":
-			finger := as[0].Finger
 			utils.DB.Model(&entity.Answer{}).Delete(&entity.Answer{}, "finger = ? and survey_id=?", finger, surveyId)
 		}
 	}
--- web/src/views/user/SurveyPage.vue
+++ web/src/views/user/SurveyPage.vue
@@ -4,8 +4,8 @@
     <el-col :span="12" :xs="24">
       <h2 class="survey-title">{{ survey.title }}</h2>
       <h4 class="survey-description">{{ survey.description }}</h4>
-      <el-form ref="answersRef" :inline-message="true" :model="form" :rules="rules" label-width="0">
-        <el-form-item :inline="false" v-for="(question , index) in survey.questions" :prop="'answers.' + question.id ">
+      <el-form ref="answersRef" :model="form" :rules="rules" label-width="0">
+        <el-form-item v-for="(question , index) in survey.questions" :prop="'answers.' + question.id ">
           <div class="question-container">
             <!--题目-->
             <el-tag v-if="question.type === 'radio'">{{ index + 1 }} [单选]</el-tag>
@@ -20,12 +20,12 @@
             <!--单选题-->
             <el-radio-group v-model="form.answers[question.id]" v-if="question.type==='radio'">
               <div style="display: flex;align-items: center" v-for="option in question.options">
-                <div style="max-width: 70%;">
+                <div style="max-width: 100%;min-width: 65%;">
                   <el-radio :label=option.label>
                     {{ option.label }} {{ option.value }}
                   </el-radio>
                 </div>
-                <div>
+                <div style="width: 35%;display: contents">
                   <el-input v-model="option['extMsg']" size="small" type="textarea"
                             :autosize="{ minRows: 1, maxRows: 3 }"
                             v-if="form.answers[question.id] && form.answers[question.id][0] === option.label &&  option.has_ext_msg === 'yes'"
@@ -36,12 +36,12 @@
             <!--多选题-->
             <el-checkbox-group v-model="form.answers[question.id]" v-if="question.type==='checkbox'">
               <div style="display: flex;align-items: center" v-for="option in question.options">
-                <div style="max-width: 70%;">
+                <div style="max-width: 100%;min-width: 65%;">
                   <el-checkbox :label=option.label>
                     {{ option.label }} {{ option.value }}
                   </el-checkbox>
                 </div>
-                <div>
+                <div style="width: 35%;display: contents">
                   <el-input v-model="option['extMsg']" size="small" type="textarea"
                             :autosize="{ minRows: 1, maxRows: 3 }"
                             v-if="form.answers[question.id] && form.answers[question.id].toString().indexOf(option.label)>-1 && option.has_ext_msg==='yes'"
@@ -51,7 +51,7 @@
             </el-checkbox-group>
           </div>
         </el-form-item>
-        <el-form-item v-if="survey.need_contact==='yes'" label="联系方式:" label-width="25%" prop="contact">
+        <el-form-item style="width: 80%;" v-if="survey.need_contact==='yes'" label="联系方式:" label-width="30%" prop="contact">
           <el-input v-model="form.contact" placeholder="请填写联系方式"></el-input>
         </el-form-item>
       </el-form>
@@ -81,7 +81,7 @@ const survey = ref({
 })
 const answersRef = ref()
 const rules = ({
-  contact : [{required: true, message: '请输入联系方式', trigger: 'blur'}],
+  contact: [{required: true, message: '请输入联系方式', trigger: 'blur'}],
 })
 
 const form = reactive({
@@ -104,86 +104,74 @@ function initSurvey() {
 
 function submitAnswer(elForm) {
   elForm.validate((valid, fields) => {
-    if (valid) {
-      console.log('通过校验:')
-      let answerResult = []
-
-      for (const index in survey.value.questions) {
-        const question = survey.value.questions[index]
-        let options = question.options
-        switch (question.type) {
-          case 'text':
-            answerResult.push(
-                {
-                  id: 0,
-                  survey_id: surveyId,
-                  question_id: question.id,
-                  content: form.answers[question.id],
-                  contact: form.contact,
-                }
-            );
-            break;
-          case 'radio':
-            let extMsg = ''
-            for (let o of options) {
-              if (o.has_ext_msg === 'yes' && (o.extMsg === undefined || o.extMsg === '')) {
-                ElMessage.error('请填写完整[第' + (Number(index) + 1) + '题]的补充信息')
-                return
-              }
-              if (o.label === form.answers[question.id][0]) {
-                extMsg = o.extMsg
-              }
+    if (!valid) {
+      ElMessage.error('请填写完整信息');
+      return;
+    }
+    const answerResult = [];
+    const showErrorMsg = (questionIndex, label) => {
+      ElMessage.error(`请填写完整[第${questionIndex + 1}题,选项${label}]的补充信息`);
+    };
+    for (let questionIndex = 0; questionIndex < survey.value.questions.length; questionIndex++) {
+      const question = survey.value.questions[questionIndex];
+      const options = question.options;
+      const questionType = question.type;
+      const answers = form.answers[question.id];
+      if (questionType === 'text') {
+        answerResult.push({
+          id: 0,
+          question_id: question.id,
+          content: answers,
+        });
+      } else if (questionType === 'radio') {
+        let extMsg = '';
+        for (let option of options) {
+          if (option.has_ext_msg === 'yes' && (!option.extMsg || option.extMsg === '')) {
+            showErrorMsg(questionIndex, option.label);
+            return;
+          }
+          if (option.label === answers[0]) {
+            extMsg = option.extMsg;
+          }
+        }
+        answerResult.push({
+          question_id: question.id,
+          label: answers.toString(),
+          ext_msg: extMsg,
+        });
+      } else if (questionType === 'checkbox') {
+        for (let selectedLabel of answers) {
+          let extMsg = '';
+          for (let option of options) {
+            if (option.has_ext_msg === 'yes' && option.label === selectedLabel && (!option.extMsg || option.extMsg === '')) {
+              showErrorMsg(questionIndex, selectedLabel);
+              return;
             }
-            answerResult.push(
-                {
-                  id: 0,
-                  survey_id: surveyId,
-                  question_id: question.id,
-                  label: form.answers[question.id].toString(),
-                  ext_msg: extMsg,
-                  contact: form.contact,
-                }
-            );
-            break;
-          case 'checkbox':
-            const arr = form.answers[question.id]
-            for (let str of arr) {
-              let extMsg = ''
-              for (let o of options) {
-                if (o.has_ext_msg === 'yes' && o.label === str && (o.extMsg === undefined || o.extMsg === '')) {
-                  ElMessage.error('请填写完整[第' + (Number(index) + 1) + '题,选项' + o.label + ']的补充信息')
-                  return
-                }
-                if (o.label === str) {
-                  extMsg = o.extMsg
-                }
-              }
-              answerResult.push(
-                  {
-                    id: 0,
-                    survey_id: surveyId,
-                    question_id: question.id,
-                    label: str,
-                    ext_msg: extMsg,
-                    contact: form.contact,
-                  }
-              );
-
+            if (option.label === selectedLabel) {
+              extMsg = option.extMsg;
             }
+          }
+          answerResult.push({
+            question_id: question.id,
+            label: selectedLabel,
+            ext_msg: extMsg,
+          });
         }
       }
-      add(answerResult).then(res => {
-        if (res.success) {
-          ElMessage.success(res.message)
-          elForm.resetFields()
-        } else {
-          ElMessage.error(res.message)
-        }
-      })
-    } else {
-      ElMessage.error('请填写完整')
     }
-  })
+    answerResult.forEach(answer => {
+      answer.contact = form.contact;
+      answer.survey_id = surveyId;
+    });
+    add(answerResult).then(res => {
+      if (res.success) {
+        ElMessage.success(res.message);
+        elForm.resetFields();
+      } else {
+        ElMessage.error(res.message);
+      }
+    });
+  });
 }
 
 
@@ -196,14 +184,14 @@ initSurvey()
   height: auto;
   min-height: 40px;
   width: auto;
-  padding: 5px;
+  padding: 3px;
 }
 
 :deep(.el-radio) {
   height: auto;
   min-height: 40px;
   width: auto;
-  padding: 5px;
+  padding: 3px;
 }
 
 :deep(.el-radio__label) {
@@ -225,7 +213,7 @@ initSurvey()
 }
 
 :deep(.el-form-item__content) {
-  display: inline;
+  display: unset;
 }
 
 
