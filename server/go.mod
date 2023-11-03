c3cd9e366a9c7d337eff8a5470b56a12aa9b898e
sxz799 <sxz799@126.com>
2023-11-03 15:00:17 +0800

    添加 postgres 支持

--- server/go.mod
+++ server/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.2
 	gorm.io/driver/mysql v1.5.2
+	gorm.io/driver/postgres v1.5.4
 	gorm.io/driver/sqlite v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -32,6 +33,9 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
--- server/utils/db.go
+++ server/utils/db.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/viper"
 	"github.com/sxz799/surveyX/model/entity"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"log"
@@ -15,6 +16,16 @@ func InitDB() {
 	sqlType := viper.GetString("db.sqlType")
 	database := viper.GetString("db.database")
 	switch sqlType {
+	case "postgres":
+		username := viper.GetString("db.username")
+		password := viper.GetString("db.password")
+		host := viper.GetString("db.host")
+		dsn := "postgres://" + username + ":" + password + "@" + host + "/" + database
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Panicln("postgres数据库连接失败。", err)
+		}
 	case "mysql":
 		username := viper.GetString("db.username")
 		password := viper.GetString("db.password")
