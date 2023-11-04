cda89a365c9c4df60c761934e4eacb2474bb3ef0
sxz799 <sxz799@126.com>
2023-11-04 13:05:56 +0800

    添加问卷水印

--- web/vite.config-docker.js
+++ web/vite.config-docker.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode, command}) => {
@@ -14,6 +15,15 @@ export default defineConfig(({mode, command}) => {
                     rewrite: (path) => path.replace(/^\/api/, '')
                 }
             }
-        }
+        },
+        resolve: {
+            // https://cn.vitejs.dev/config/#resolve-alias
+            alias: {
+                // 设置路径
+                '~': path.resolve(__dirname, './'),
+                // 设置别名
+                '@': path.resolve(__dirname, './src')
+            },
+        },
     }
 })
--- web/vite.config.js
+++ web/vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 export default defineConfig(({mode, command}) => {
     return {
         plugins: [vue()],
+
         server: {
             proxy: {
                 '/api': {
