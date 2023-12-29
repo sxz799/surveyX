eb2cc11585e03556decfb6b3dfdb939c026d7f86
xianzhi song <sxz799@126.com>
2023-12-29 02:09:21 +0000

    打包混淆

--- web/vite.config.js
+++ web/vite.config.js
@@ -49,18 +49,9 @@ export default defineConfig(({ mode, command }) => {
                             return id.toString().split("node_modules/")[1].split("/")[0].toString()
                         }
                     },
-                    // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
-                    entryFileNames: 'js/[name].[hash].js',
-                    // 用于命名代码拆分时创建的共享块的输出命名
-                    // 　　chunkFileNames: 'js/[name].[hash].js',
-                    // 用于输出静态资源的命名，[ext]表示文件扩展名
-                    assetFileNames: '[ext]/[name].[hash].[ext]',
-                    // 拆分js到模块文件夹
-                    chunkFileNames: (chunkInfo) => {
-                        const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
-                        const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
-                        return `js/${fileName}/[name].[hash].js`;
-                    },
+                    chunkFileNames: 'js/[hash].js',
+                    entryFileNames: 'js/[hash].js',
+                    assetFileNames: '[ext]/[hash].[ext]',
                 }
             }
         },
