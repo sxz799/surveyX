178dc8eb7443aade6d433976561e77a2408b8467
sxz799 <sxz799@126.com>
2024-04-09 20:24:14 +0800

    add:RegWebRouter

--- server/main.go
+++ server/main.go
@@ -8,18 +8,11 @@ import (
 	"github.com/sxz799/surveyX/config"
 	"github.com/sxz799/surveyX/router"
 	"github.com/sxz799/surveyX/utils"
-	"html/template"
-	"io/fs"
 	"log"
-	"net/http"
 )
-
-// ------前后端分离调试时请注释下面代码------
 
 //go:embed dist
 var content embed.FS
-
-//------前后端分离调试时请注释上面代码------
 
 func main() {
 	config.Init()
@@ -33,18 +26,8 @@ func startGin() {
 	r.Use(cors.Default())
 	r.Use(gzip.Gzip(gzip.DefaultCompression))
 
-	//------前后端分离调试时请注释下面代码------
-	temp := template.Must(template.New("").ParseFS(content, "dist/index.html"))
-	r.SetHTMLTemplate(temp)
-	distFS, _ := fs.Sub(content, "dist")
-	r.StaticFS("/dist", http.FS(distFS))
-	r.NoRoute(func(context *gin.Context) {
-		context.HTML(200, "index.html", "")
-	})
-	log.Println("已开启前后端整合模式！")
-	//------前后端分离调试时请注释上面代码------
-
 	router.RegRouter(r)
+	router.RegWebRouter(r, content)
 	log.Println("服务启动中,当前使用端口：", config.Port)
 
 	err := r.Run(":" + config.Port)
--- server/router/enter.go
+++ server/router/enter.go
@@ -1,11 +1,16 @@
 package router
 
 import (
+	"embed"
 	"github.com/gin-gonic/gin"
 	"github.com/sxz799/surveyX/router/answer"
 	"github.com/sxz799/surveyX/router/common"
 	"github.com/sxz799/surveyX/router/question"
 	"github.com/sxz799/surveyX/router/survey"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
 )
 
 func RegRouter(e *gin.Engine) {
@@ -13,4 +18,15 @@ func RegRouter(e *gin.Engine) {
 	question.Question(e)
 	answer.Answer(e)
 	common.Common(e)
+}
+
+func RegWebRouter(e *gin.Engine, content embed.FS) {
+	temp := template.Must(template.New("").ParseFS(content, "dist/index.html"))
+	e.SetHTMLTemplate(temp)
+	distFS, _ := fs.Sub(content, "dist")
+	e.StaticFS("/dist", http.FS(distFS))
+	e.NoRoute(func(context *gin.Context) {
+		context.HTML(200, "index.html", "")
+	})
+	log.Println("已开启前后端整合模式！")
 }
